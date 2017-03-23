5ec22bf4697e95665132380574fcfa783a0a322a
Unknown <mshimada2@mail.csuchico.edu>
2017-03-23 13:42:56 -0700

    i added the ability to approve passwords and usernames as soon as they are entered and without having to press register; i put in an alert when you pressed register

--- form-validation.js
+++ form-validation.js
@@ -1,7 +1,14 @@
-function validate(){
+function register(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
+  alert(userEntered +" "+ passEntered);
+
+}
+
+function validateUsername(){
+
+  var userEntered = document.getElementById("user").value;
 
   if (userEntered.length < 6){
     document.getElementById("usernameError").innerHTML="Bad username.";
@@ -11,61 +18,64 @@ function validate(){
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
-else {
+  else {
   document.getElementById("usernameError").innerHTML="Good username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
+  document.getElementById("usernameError").classList.add("hidden-message");
+  document.getElementById("usernameError").classList.remove("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-success");
     document.getElementById("usernameGroup").classList.remove("has-error");
+  }
 }
 
 
-console.log(passEntered);
+function validatePassword(){
 
-if (passEntered == "password"){
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
-}
+  var userEntered = document.getElementById("user").value;
+  var passEntered = document.getElementById("pass").value;
 
+  console.log(passEntered);
 
-else if (passEntered.length < 6){
-  document.getElementById("passwordError").innerHTML="Password Must Be At Least 6 Charachters.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
-}
-else if (passEntered.length > 20){
-  document.getElementById("passwordError").innerHTML="Password Must Be Less Than 20 Charachters.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
+  if (passEntered == "password"){
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
     document.getElementById("passwordGroup").classList.add("has-error");
-}
-
-else if (passEntered == userEntered){
-  document.getElementById("passwordError").innerHTML="Password Cannot Be The Same As Username";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
-}
-
-else {
-  document.getElementById("passwordError").innerHTML="Good Password";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-success");
-  document.getElementById("passwordGroup").classList.remove("has-error");
+  }
 
 
+  else if (passEntered.length < 6){
+    document.getElementById("passwordError").innerHTML="Password Must Be At Least 6 Charachters.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else if (passEntered.length > 20){
+    document.getElementById("passwordError").innerHTML="Password Must Be Less Than 20 Charachters.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+      document.getElementById("passwordGroup").classList.add("has-error");
+  }
 
-}
+  else if (passEntered == userEntered){
+    document.getElementById("passwordError").innerHTML="Password Cannot Be The Same As Username";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
 
+  else {
+    document.getElementById("passwordError").innerHTML="Good Password";
+    document.getElementById("passwordError").classList.add("hidden-message");
+    document.getElementById("passwordError").classList.remove("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-success");
+    document.getElementById("passwordGroup").classList.remove("has-error");
+
+  }
 
 }
